374f2e63392ca5f873b88d2ccbde12bdb2669d7c
stellaartois901 <85124355+stellaartois901@users.noreply.github.com>
2021-05-31 09:59:49 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
-// Select color input
-// Select size input
+const table = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+const sizePicker = document.getElementById('sizePicker');
 
-function makeGrid() {
+sizePicker.addEventListener('submit', function(event) {
+	event.preventDefault();
+	const height = document.getElementById('inputHeight').value;
+	const width = document.getElementById('inputWidth').value;
+	makeGrid(height, width)
+})
 
 // Your code goes here!
 
+function makeGrid(height, width) {
+    table.innerHTML = "";
+    for (let y=0; y < height; y++) {
+        let row = table.insertRow(y);
+        for (let x=0; x < width; x++) {
+            let cell = row.insertCell(x);
+            // color changes on click
+            cell.addEventListener('click', function(event) {
+              const element = event.target;
+              // get color
+              const color = document.getElementById('colorPicker').value;
+              // paint cell
+              element.style.backgroundColor = color;
+            });
+        }
+    }
 }
